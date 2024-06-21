212e538e5210c4a7977f23429585a4b3575b3947
Jessica <laineandjessica@gmail.com>
2024-06-21 14:05:31 -0500

    Started basic character movement and speed

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    public float hortizontalInput;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hortizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(hortizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        
+    }
+}
